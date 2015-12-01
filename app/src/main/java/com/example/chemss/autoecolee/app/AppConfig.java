feb54b3504d1902c83b8f350e2ae5ddb93f09f5a
bychems <byoussefchems@gmail.com>
2015-12-01 21:16:05 +0100

    nnn

---
+++ app/src/main/java/com/example/chemss/autoecolee/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.chemss.autoecolee.app;
+
+/**
+ * Created by chemss on 30/11/2015.
+ */
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.56.1:90/WebService/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.56.1:90/WebService/register.php";
+}
